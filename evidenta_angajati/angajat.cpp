b88566de64d1dc64d2d34a5887ee3c6233baa4e0
HDiana10 <68329980+HDiana10@users.noreply.github.com>
2025-07-16 15:54:14 +0300

    Create angajat.cpp

---
+++ evidenta_angajati/angajat.cpp
@@ -0,0 +1,8 @@
+#include "Angajat.h"
+#include <string>
+
+Angajat::Angajat(std::string tip, std::string nume, std::string departament):tip(tip), nume(nume), departament(departament){}
+
+std::string Angajat::get_nume(){return nume;}
+std::string Angajat::get_departament(){return departament;}
+//std::string Angajat::get_tip(){return tip;}
